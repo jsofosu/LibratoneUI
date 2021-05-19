6456357a38ad1d8d16fc9aee331c4edad63ef299
jsofosu <61045983+jsofosu@users.noreply.github.com>
2021-05-19 03:12:35 +0000

    update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import'package:music/screens/home.dart';
+import 'package:music/screens/home.dart';
 
 void main() {
   runApp(MyApp());
@@ -10,11 +10,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
-       
         primarySwatch: Colors.blue,
-       
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       home: Home(),
